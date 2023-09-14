acff7e7c67d7183b06d3fa5893274d41ec42d34c
Prithivraj Gankai R <prithivrajgankai@gmail.com>
2023-09-14 18:32:44 +0530

    Add files via upload

---
+++ exam-init.sh
@@ -0,0 +1,14 @@
+#/bin/bash
+
+tar -xvf Rhce.tar  > /dev/null 2>&1
+
+echo -ne '#####                      (33%)\r'
+
+dnf install httpd -y     > /dev/null 2>&1
+systemctl enable --now  httpd   > /dev/null 2>&1
+
+echo -ne '##############             (50%)\r'
+
+cp -rf Rhce  /var/www/html/Rhce     > /dev/null 2>&1
+systemctl  restart httpd   > /dev/null 2>&1
+echo -ne '###########################(100%)\n'
