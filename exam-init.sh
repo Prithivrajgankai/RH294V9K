8515782f43b0d115699c6e44ae832730670c0e59
Prithivraj Gankai R <prithivrajgankai@gmail.com>
2026-09-12 18:46:02 +0530

    Update exam-init.sh

--- exam-init.sh
+++ exam-init.sh
@@ -11,5 +11,7 @@ systemctl disable --now  firewalld   > /dev/null 2>&1
 echo -ne '##############             (50%)\r'
 
 cp -rf Rhce  /var/www/html/Rhce     > /dev/null 2>&1
+mkdir /var/www/html/Rhce/redhat-rhel_system_roles     > /dev/null 2>&1
+tar -zxf /var/www/html/Rhce/redhat-rhel_system_roles-1.0.0.tar.gz -C /var/www/html/Rhce/redhat-rhel_system_roles/    > /dev/null 2>&1
 systemctl  restart httpd   > /dev/null 2>&1
 echo -ne '###########################(100%)\n'
